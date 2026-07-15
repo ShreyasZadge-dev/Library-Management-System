f9d2f14676fd6e9ba0228a0d14ec5ddfd4d9d282
Shreyas Zadge <shreyaszadge@gmail.com>
2026-07-15 14:12:11 +0530

    Custome Exception and String Pattern added

---
+++ src/pkg_exception/StudentNotFoundException.java
@@ -0,0 +1,13 @@
+package pkg_exception;
+
+
+public class StudentNotFoundException extends Exception {
+
+    public StudentNotFoundException(){}
+
+    @Override
+    public String toString() {
+        return "StudentNotFoundException is Generated";
+    }
+    
+}
--- src/pkg_person/Person.java
+++ src/pkg_person/Person.java
@@ -1,50 +1,74 @@
 package pkg_person;
 
+import java.util.regex.Pattern;
+
 abstract public class Person {
     protected String name;
     protected String emailId;
     protected String phonenumber;
     protected String address;
     protected String dob;
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
+        boolean isValidName = Pattern.matches("[a-zA-Z]+", name);
         this.name = name;
+        if (isValidName) {
+            this.name = name;
+        } else {
+            this.name = "Default Name";
+        }
+
     }
+
     public String getEmailId() {
         return emailId;
     }
+
     public void setEmailId(String emailId) {
         this.emailId = emailId;
     }
+
     public String getPhonenumber() {
         return phonenumber;
     }
+
     public void setPhonenumber(String phonenumber) {
         this.phonenumber = phonenumber;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getDob() {
         return dob;
     }
+
     public void setDob(String dob) {
-        this.dob = dob;
+        boolean isValidDob = Pattern.matches("\\d{2}-\\d{2}\\d{4}", dob);
+        if (isValidDob) {
+            this.dob = dob;
+        }
+        this.dob = "01-06-2005";
     }
+
     public Person(String name, String emailId, String phonenumber, String address, String dob) {
-        this.name = name;
+        this.setName(name);
         this.emailId = emailId;
         this.phonenumber = phonenumber;
         this.address = address;
-        this.dob = dob;
+        this.setDob(dob);
     }
+
     public Person() {
     }
 
-    
 }
